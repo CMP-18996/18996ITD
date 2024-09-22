a9ed1e4b7b2cb3f4418c4d0690af625886ccac53
donaldzhuang <donaldzhuang08@gmail.com>
2024-09-21 21:22:41 -0400

    Trapdoor command and enums in intake subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commands/LiftTrapdoorCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commands/LiftTrapdoorCommand.java
@@ -1,4 +1,15 @@
 package org.firstinspires.ftc.teamcode.common.commands;
 
-public class LiftTrapdoorCommand {
+import com.arcrobotics.ftclib.command.InstantCommand;
+
+import org.firstinspires.ftc.teamcode.common.robot.subsystems.IntakeSubsystem;
+
+
+public class LiftTrapdoorCommand extends InstantCommand{
+
+    public LiftTrapdoorCommand(IntakeSubsystem intakeSubsystem, IntakeSubsystem.TrapdoorState state){
+        super(
+                () -> intakeSubsystem.UpdateTrapdoorState(state)
+        );
+    }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/robot/subsystems/IntakeSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/robot/subsystems/IntakeSubsystem.java
@@ -7,16 +7,40 @@ import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class IntakeSubsystem extends SubsystemBase {
-    double trapdoorMinRot;
-    double trapdoorMaxRot;
-    double intakeRotationMinRot;
-    double intakeRotationMaxRot;
+    final double TRAPDOOR_MIN_ROT = 0.0;
+    final double TRAPDOOR_MAX_ROT = 0.0;
+    final double INTAKE_ROTATION_MIN_ROT = 0.0;
+    final double INTAKE_ROTATION_MAX_ROT = 0.0;
     CRServo intakeServo1, intakeServo2;
     ServoEx trapdoorServo, intakeRotationServo;
+    public static double CLOSED_VALUE = 0.1;
+    public static double EJECTING_VALUE = 10.0;
+    public static double TRANSFERRING_VALUE = 1.0;
+    public static double ACTIVE_VALUE = 1.0;
+    public static double DISABLED_VALUE = 0.0;
+    private TrapdoorState trapdoorState = TrapdoorState.CLOSED;
+    public enum TrapdoorState {
+        CLOSED(CLOSED_VALUE),
+        EJECTING(EJECTING_VALUE),
+        TRANSFERRING(TRANSFERRING_VALUE);
+        public double trapdoorValue;
+        TrapdoorState(double val){trapdoorValue = val;}
+
+    }
+    public enum IntakingState {
+        ACTIVE(ACTIVE_VALUE),
+        DISABLED(DISABLED_VALUE);
+        public double intakingValue;
+        IntakingState(double val){intakingValue = val;}
+    }
     public IntakeSubsystem(HardwareMap hardwareMap) {
-        trapdoorServo = new SimpleServo(hardwareMap, "trapdoor", trapdoorMinRot, trapdoorMaxRot);
-        intakeRotationServo = new SimpleServo(hardwareMap, "intakeRotator", intakeRotationMinRot, intakeRotationMaxRot);
+        trapdoorServo = new SimpleServo(hardwareMap, "trapdoor", TRAPDOOR_MIN_ROT, TRAPDOOR_MAX_ROT);
+        intakeRotationServo = new SimpleServo(hardwareMap, "intakeRotator", INTAKE_ROTATION_MIN_ROT, INTAKE_ROTATION_MAX_ROT);
         intakeServo1 = hardwareMap.get(CRServo.class, "intake1");
         intakeServo2 = hardwareMap.get(CRServo.class, "intake2");
+    }
+    public void UpdateTrapdoorState(TrapdoorState setState) {
+        trapdoorState = setState;
+        trapdoorServo.setPosition(trapdoorState.trapdoorValue);
     }
 }
