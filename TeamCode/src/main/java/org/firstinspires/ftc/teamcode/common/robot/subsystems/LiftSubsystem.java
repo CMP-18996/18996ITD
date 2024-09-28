d3190ff21772e21b4f5de90ed70fb3e6f52b4ff4
christopheraustin <arcticplayernoice@gmail.com>
2024-09-28 18:35:07 -0400

    Updated Lift and Deposit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/robot/subsystems/LiftSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/robot/subsystems/LiftSubsystem.java
@@ -1,10 +1,36 @@
 package org.firstinspires.ftc.teamcode.common.robot.subsystems;
 
 import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorImpl;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class LiftSubsystem extends SubsystemBase {
-    public LiftSubsystem(HardwareMap hardwareMap) {
+    // Constants
+    private final double P = .0001;
 
+    // State
+    private final DcMotorImpl liftMotor;
+    private int currTarget = 0;
+    private int currPosition = 0;
+
+    public void setTargetPosition(int targetPosition) {
+        currTarget = targetPosition; // TODO: Replace with actual states
+    }
+
+    @Override
+    public void periodic() {
+        currPosition = liftMotor.getCurrentPosition();
+        liftMotor.setTargetPosition(currTarget);
+        liftMotor.setPower(P * Math.abs(currPosition - currTarget)); // TODO: implement proportional controller
+    }
+
+    public LiftSubsystem(HardwareMap hardwareMap) {
+        liftMotor = hardwareMap.get(DcMotorImpl.class, "liftMotor");
+        liftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        liftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        currPosition = liftMotor.getCurrentPosition();
     }
 }
