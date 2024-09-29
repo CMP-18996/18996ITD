0257ab391a985c9a69a8ca8527e109a413b0d704
donaldzhuang <donaldzhuang08@gmail.com>
2024-09-28 20:05:52 -0400

    Formatting changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commands/LiftSetPosition.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commands/LiftSetPosition.java
@@ -7,19 +7,20 @@ import org.firstinspires.ftc.teamcode.common.robot.subsystems.LiftSubsystem;
 public class LiftSetPosition extends CommandBase {
     LiftSubsystem liftSubsystem;
     int target;
-        public LiftSetPosition(LiftSubsystem liftSubsystem, int targetPosition){
-            target = targetPosition;
-            this.liftSubsystem = liftSubsystem;
-            addRequirements(liftSubsystem);
-            }
 
-        @Override
-        public void initialize() {
-            liftSubsystem.setTargetPosition(target);
-        }
+    public LiftSetPosition(LiftSubsystem liftSubsystem, int targetPosition){
+        target = targetPosition;
+        this.liftSubsystem = liftSubsystem;
+        addRequirements(liftSubsystem);
+    }
 
-        @Override
-        public boolean isFinished() {
-            return liftSubsystem.motorWorking();
-        }
+    @Override
+    public void initialize() {
+        liftSubsystem.setTargetPosition(target);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return liftSubsystem.motorWorking();
+    }
 }
