dbbb614354db0cbb9f2e25ef2448a2d9bb8d3288
artica1 <arjun.sieracki@icloud.com>
2024-11-11 20:36:17 -0500

    Fixed some of my stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/HardwareTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/HardwareTeleop.java
@@ -1,8 +1,5 @@
 package org.firstinspires.ftc.teamcode.teleops;
 
-import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_TO_POSITION;
-import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_USING_ENCODER;
-
 import com.acmerobotics.dashboard.config.Config;
 import com.arcrobotics.ftclib.controller.PIDFController;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -14,42 +11,66 @@ import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+import org.firstinspires.ftc.teamcode.common.robot.Drive;
 import org.firstinspires.ftc.teamcode.common.robot.HardwareMapNames;
+import org.firstinspires.ftc.teamcode.odo.GoBildaPinpointDriver;
+import org.firstinspires.ftc.teamcode.odo.OdometryHardware;
 
 @TeleOp(name = "HardwareTeleop")
 @Config
 public class HardwareTeleop extends LinearOpMode {
 
-    public static double INTAKE_SERVO_SPEED = 1.0;
+    public static double INTAKE_SERVO_SPEED = -1.0;
 
     public static double INTAKE_ROTATOR_ACTIVE = 0.5;
     public static double INTAKE_ROTATOR_INACTIVE = 0.0;
-    public static double INTAKE_ROTATOR_OFFSET = 0.01;
+    public static double INTAKE_ROTATOR_OFFSET = 0.05;
 
     public static double BUCKET_SERVO_IDLE = 0.0;
-    public static double BUCKET_SERVO_HALF = 0.2;
-    public static double BUCKET_SERVO_FULL = 0.4;
+    public static double BUCKET_SERVO_HALF = 0.05;
+    public static double BUCKET_SERVO_FULL = 0.13;
 
     public static double SPECIMEN_OPEN = 0.0;
-    public static double SPECIMEN_CLOSED = 0.5;
+    public static double SPECIMEN_CLOSED = 1.0;
 
     //public static int LIFT_ZERO_POSITION = 0;
-    public static int LIFT_LOW_CHAMBER = 500; //idfk
-    public static int LIFT_HIGH_CHAMBER = 1500; //idfk
-    public static int LIFT_LOW_BUCKET = 1000;
-    public static int LIFT_HIGH_BUCKET = 2000;
+    public static int LIFT_LOW_CHAMBER = 250; //idfk
+    public static int LIFT_HIGH_CHAMBER = 650; //idfk
+    public static int LIFT_LOW_BUCKET = 450;
+    public static int LIFT_HIGH_BUCKET = 850;
+    // 100
 
-    public static double kP = 0.001;
+    public static double kP = 0.01;
     public static double kI = 0.0;
-    public static double kD = 0.0;
+    public static double kD = 0.000;
     public static double kF = 0.0;
     public static double LIFT_TOLERANCE = 10;
 
-    public void runOpMode() {
+    public enum COLOR {
+        RED,
+        YELLOW,
+        BLUE;
+    }
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private Drive drive;
+    private OdometryHardware odometryHardware;
+
+    public void runOpMode() throws InterruptedException{
         Gamepad currentGamepad1 = new Gamepad();
 
         Gamepad previousGamepad1 = new Gamepad();
+
+        drive = new Drive(hardwareMap);
+        drive.setDriveMode(Drive.DriveMode.FIELD_CENTRIC);
+
+        odometryHardware = new OdometryHardware(hardwareMap);
 
         boolean intakeActive = false;
 
@@ -61,14 +82,18 @@ public class HardwareTeleop extends LinearOpMode {
 
         double rotatorOffset = 0;
 
+        double turnOffTime = 0;
+
         //int liftTarget = 0;
 
         Servo intakeRotator = hardwareMap.get(Servo.class, HardwareMapNames.INTAKE_ROTATOR);
         intakeRotator.scaleRange(0.0, 1.0);
+        intakeRotator.setDirection(Servo.Direction.REVERSE);
 
         CRServo intake1 = hardwareMap.get(CRServo.class, HardwareMapNames.INTAKE_SERVO_1);
         CRServo intake2 = hardwareMap.get(CRServo.class, HardwareMapNames.INTAKE_SERVO_2);
         intake1.setDirection(DcMotorSimple.Direction.REVERSE);
+        intake2.setDirection(DcMotorSimple.Direction.FORWARD);
 
         ColorSensor colorSensor = hardwareMap.get(ColorSensor.class, HardwareMapNames.INTAKE_COLOR_SENSOR);
 
@@ -80,6 +105,9 @@ public class HardwareTeleop extends LinearOpMode {
         //liftMotor.setTargetPosition(0);
         //liftMotor.setMode(RUN_TO_POSITION);
         liftMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        liftMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        liftMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        liftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
         PIDFController liftPIDF = new PIDFController(kP, kI, kD, kF);
         liftPIDF.setTolerance(LIFT_TOLERANCE);
@@ -88,7 +116,7 @@ public class HardwareTeleop extends LinearOpMode {
         bucketServo.scaleRange(0.0, 1.0);
 
         Servo specimenServo = hardwareMap.get(Servo.class, HardwareMapNames.SPECIMEN_SERVO);
-        specimenServo.scaleRange(0.0, 1.0);
+        specimenServo.scaleRange(0.6, 0.8);
 
         DcMotor leftFrontDrive = hardwareMap.get(DcMotor.class, HardwareMapNames.LEFT_FRONT);
         DcMotor leftBackDrive = hardwareMap.get(DcMotor.class, HardwareMapNames.LEFT_BACK);
@@ -102,6 +130,8 @@ public class HardwareTeleop extends LinearOpMode {
 
         bucketServo.setPosition(BUCKET_SERVO_IDLE);
         specimenServo.setPosition(SPECIMEN_CLOSED);
+        intakeRotator.setPosition(INTAKE_ROTATOR_ACTIVE/2);
+       // wait(100);
         intakeRotator.setPosition(INTAKE_ROTATOR_INACTIVE);
         intake1.setPower(0);
         intake2.setPower(0);
@@ -111,10 +141,48 @@ public class HardwareTeleop extends LinearOpMode {
         telemetry.update();
 
         waitForStart();
+        runtime.reset();
 
         while (opModeIsActive()) {
             previousGamepad1.copy(currentGamepad1);
             currentGamepad1.copy(gamepad1);
+
+            // COLOR
+            double r, g, b;
+            r = colorSensor.red();
+            g = colorSensor.green();
+            b = colorSensor.blue();
+
+            COLOR color;
+
+            if (colorSensor.alpha() > 100) {
+                if (r > g && r > b) {
+                    color = COLOR.RED;
+                } else if (g > r && g > b) {
+                    color = COLOR.YELLOW;
+                } else if (b > g && b > r) {
+                    color = COLOR.BLUE;
+                }
+            }
+            /*
+            YELLOW
+            402
+            544
+            125
+            359
+
+            RED
+            253
+            140
+            66
+            157
+
+            BLUE
+            72
+            152
+            335
+            186
+             */
 
             //
             // INTAKE
@@ -122,27 +190,46 @@ public class HardwareTeleop extends LinearOpMode {
             if (currentGamepad1.right_bumper && !previousGamepad1.right_bumper) {
                 intakeActive = !intakeActive;
 
-                if (intakeActive) {
-                    intake1.setPower(INTAKE_SERVO_SPEED);
-                    intake2.setPower(INTAKE_SERVO_SPEED);
-                }
-                else {
-                    intake1.setPower(0);
-                    intake2.setPower(0);
-
-                    intakeRotator.setPosition(INTAKE_ROTATOR_INACTIVE);
+                if (!intakeActive) {
+                    turnOffTime = runtime.milliseconds() + 1000;
                 }
             }
+
+            //if (color = COLOR.GREEN)
 
             if (intakeActive) {
                 rotatorOffset = -currentGamepad1.right_stick_y * INTAKE_ROTATOR_OFFSET;
+                rotatorOffset = 0;
                 intakeRotator.setPosition(INTAKE_ROTATOR_ACTIVE + rotatorOffset);
+
+                if (currentGamepad1.left_bumper) {
+                    intake1.setPower(-INTAKE_SERVO_SPEED);
+                    intake2.setPower(-INTAKE_SERVO_SPEED);
+                }
+                else {
+                    intake1.setPower(INTAKE_SERVO_SPEED);
+                    intake2.setPower(INTAKE_SERVO_SPEED);
+                }
+            }
+            else {
+                if (currentGamepad1.left_bumper) {
+                    intake1.setPower(-INTAKE_SERVO_SPEED);
+                    intake2.setPower(-INTAKE_SERVO_SPEED);
+                }
+                else {
+                    if (runtime.milliseconds() > turnOffTime) {
+                        intake1.setPower(0);
+                        intake2.setPower(0);
+                    }
+                    else {
+                        intake1.setPower(INTAKE_SERVO_SPEED);
+                        intake2.setPower(INTAKE_SERVO_SPEED);
+                    }
+                }
+
+                intakeRotator.setPosition(INTAKE_ROTATOR_INACTIVE);
             }
 
-            if (currentGamepad1.left_bumper) {
-                intake1.setPower(1.0);
-                intake2.setPower(1.0);
-            }
 
             //
             // EXTENSION (cubic control)
@@ -203,12 +290,14 @@ public class HardwareTeleop extends LinearOpMode {
             }
 
             // lift PID
-            liftPower = liftPIDF.calculate(liftMotor.getCurrentPosition());
+            liftPower = liftPIDF.calculate(-liftMotor.getCurrentPosition());
             if (Math.abs(liftPower) > 1) {
                 liftPower /= Math.abs(liftPower);
                 telemetry.addLine("EXCEED POWER !!!!");
             }
-            liftMotor.setPower(liftPower);
+            liftMotor.setPower(liftPower - currentGamepad1.right_stick_y);
+
+
 
             // bucket
             if (currentGamepad1.cross && !previousGamepad1.cross) {
@@ -248,53 +337,15 @@ public class HardwareTeleop extends LinearOpMode {
             //
             // DRIVE
             //
-            double x =  currentGamepad1.left_stick_x;
-            double y   = -currentGamepad1.left_stick_y;
-            double turn =  currentGamepad1.right_stick_x;
+            odometryHardware.pinpoint.update(GoBildaPinpointDriver.readData.ONLY_UPDATE_HEADING);
+            double heading = odometryHardware.pinpoint.getHeading();
+            drive.vectorDrive(gamepad1.left_stick_x, -gamepad1.left_stick_y, gamepad1.right_stick_x, heading);
 
-            /*
-            double theta = Math.atan2(y, x);
-            double power = Math.hypot(x, y);
-
-            double sin = Math.sin(theta - Math.PI/4);
-            double cos = Math.cos(theta - Math.PI/4);
-            double max = Math.max(Math.abs(sin), Math.abs(cos));
-
-            double leftFrontPower  = power + cos/max + turn;
-            double rightFrontPower = power - sin/max - turn;
-            double leftBackPower   = power - sin/max + turn;
-            double rightBackPower  = power + cos/max - turn;
-
-            if ((power + Math.abs(turn)) > 1) {
-                leftFrontPower  /= power + turn;
-                rightFrontPower /= power + turn;
-                leftBackPower   /= power + turn;
-                rightBackPower  /= power + turn;
-            }
-            */
-            double max;
-            double leftFrontPower  = y + x + turn;
-            double rightFrontPower = y - x - turn;
-            double leftBackPower   = y - x + turn;
-            double rightBackPower  = y + x - turn;
-
-            // Normalize the values so no wheel power exceeds 100%
-            // This ensures that the robot maintains the desired motion.
-            max = Math.max(Math.abs(leftFrontPower), Math.abs(rightFrontPower));
-            max = Math.max(max, Math.abs(leftBackPower));
-            max = Math.max(max, Math.abs(rightBackPower));
-
-            if (max > 1.0) {
-                leftFrontPower  /= max;
-                rightFrontPower /= max;
-                leftBackPower   /= max;
-                rightBackPower  /= max;
+            if (gamepad1.options) {
+                odometryHardware.pinpoint.setPosition(new Pose2D(DistanceUnit.INCH, 0, 0, AngleUnit.RADIANS, 0));
+                telemetry.addLine("Reset Heading");
             }
 
-            leftFrontDrive.setPower(leftFrontPower);
-            rightFrontDrive.setPower(rightFrontPower);
-            leftBackDrive.setPower(leftBackPower);
-            rightBackDrive.setPower(rightBackPower);
 
             telemetry.addData("INTAKE ACTIVE", intakeActive);
             telemetry.addData("INTAKE OFFSET", rotatorOffset);
@@ -317,6 +368,8 @@ public class HardwareTeleop extends LinearOpMode {
             telemetry.addData("BLUE", colorSensor.blue());
             telemetry.addData("ALPHA", colorSensor.alpha());
             telemetry.addLine();
+
+            telemetry.addData("HEADING", Math.toDegrees(heading));
 
             telemetry.update();
         }
