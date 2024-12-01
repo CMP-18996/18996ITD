caa50698320bf9283c251c5a2f5c90596597de33
artica1 <arjun.sieracki@icloud.com>
2024-11-30 19:09:13 -0500

    Added Team to Robot Class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/robot/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/robot/Robot.java
@@ -18,9 +18,12 @@ public class Robot {
     public LiftSubsystem lift;
     public SpecimenSubsystem specimen;
     public HardwareMap hardwareMap;
+    private final Team team;
 
+    @Deprecated
     public Robot(HardwareMap hardwareMap, Subsystems... subsystems) {
         this.hardwareMap = hardwareMap;
+        this.team = null;
         for (Subsystems subsystem : subsystems) {
             if (subsystem == Subsystems.ALL) {
                 hang = new HangSubsystem(hardwareMap);
@@ -49,6 +52,43 @@ public class Robot {
                 intake = new IntakeSubsystem(hardwareMap);
             }
         }
+    }
+
+    public Robot(HardwareMap hardwareMap, Team team, Subsystems... subsystems) {
+        this.hardwareMap = hardwareMap;
+        this.team = team;
+        for (Subsystems subsystem : subsystems) {
+            if (subsystem == Subsystems.ALL) {
+                hang = new HangSubsystem(hardwareMap);
+                extension = new ExtensionSubsystem(hardwareMap);
+                deposit = new DepositSubsystem(hardwareMap);
+                lift = new LiftSubsystem(hardwareMap);
+                intake = new IntakeSubsystem(hardwareMap);
+                specimen = new SpecimenSubsystem(hardwareMap);
+            }
+            else if (subsystem == Subsystems.HANG) {
+                hang = new HangSubsystem(hardwareMap);
+            }
+            else if (subsystem == Subsystems.EXTENSION) {
+                extension = new ExtensionSubsystem(hardwareMap);
+            }
+            else if (subsystem == Subsystems.DEPOSIT) {
+                deposit = new DepositSubsystem(hardwareMap);
+            }
+            else if (subsystem == Subsystems.LIFT) {
+                lift = new LiftSubsystem(hardwareMap);
+            }
+            else if (subsystem == Subsystems.SPECIMEN) {
+                specimen = new SpecimenSubsystem(hardwareMap);
+            }
+            else if (subsystem == Subsystems.INTAKE) {
+                intake = new IntakeSubsystem(hardwareMap);
+            }
+        }
+    }
+
+    public Team getTeam() {
+        return team;
     }
 }
 
