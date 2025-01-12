62b50c1b8ce1afadb372ccd1bdbd3adc06ae3510
Arjun <arjun.sieracki@icloud.com>
2025-01-12 13:38:25 -0500

    STATICLocalizer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/robot/OdometryHardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/robot/OdometryHardware.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode.common.robot;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.pedropathing.localization.GoBildaPinpointDriver;
 import com.qualcomm.hardware.limelightvision.LLResult;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.hardware.sparkfun.SparkFunOTOS;
@@ -10,7 +11,6 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
-import org.firstinspires.ftc.teamcode.common.odo.GoBildaPinpointDriver;
 
 
 /*
