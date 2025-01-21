7507e0c86c581e18bb182487e71b3592228a3f40
donaldzhuang <donaldzhuang08@gmail.com>
2025-01-20 19:34:55 -0500

    changes of the extension pid

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/bigcommands/RetractAndTransferCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/bigcommands/RetractAndTransferCommand.java
@@ -1,8 +1,14 @@
-package org.firstinspires.ftc.teamcode.common.commands;
+package org.firstinspires.ftc.teamcode.common.bigcommands;
 
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 import com.arcrobotics.ftclib.command.WaitCommand;
 
+import org.firstinspires.ftc.teamcode.common.commands.DepositRotationCommand;
+import org.firstinspires.ftc.teamcode.common.commands.ExtendCommand;
+import org.firstinspires.ftc.teamcode.common.commands.IntakeArmPivotCommand;
+import org.firstinspires.ftc.teamcode.common.commands.IntakeCommand;
+import org.firstinspires.ftc.teamcode.common.commands.IntakeDirectPivotCommand;
+import org.firstinspires.ftc.teamcode.common.commands.IntakeRotatorCommand;
 import org.firstinspires.ftc.teamcode.common.robot.subsystems.DepositSubsystem;
 import org.firstinspires.ftc.teamcode.common.robot.subsystems.ExtensionSubsystem;
 import org.firstinspires.ftc.teamcode.common.robot.subsystems.IntakeSubsystem;
